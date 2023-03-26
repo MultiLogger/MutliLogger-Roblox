c010143f3ab3b0bafa95e1e94583bc780d87a66a
MultiLogger <128045580+MultiLogger@users.noreply.github.com>
2023-03-25 20:33:40 -0400

    Update table.lua

--- table.lua
+++ table.lua
@@ -1,10 +1,10 @@
--- // Table of Tool
+-- // Table of tool
 
 getgenv().MutliLogger = {
     Webhook = {
-        ["URL"] = "https://discord.com/api/webhooks/1085092116448952461/DHQ37Am32EvoUx4uiGbDbWamWxDM1eR7GcHropBW5Lrfk_PP0zGH927wes_hH6RjJw9Z"
+        ["URL"] = "YOUR_WEBHOOK_HERE"
     },
     Text = {
-        ["CustomText"] = "i love logging robloxians"
+        ["CustomText"] = "CUSTOM_TEXT_HERE"
     }
 }
